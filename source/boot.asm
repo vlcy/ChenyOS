267a8739e3ff730e9f0266252dccf8a169a38e77
vlcy <vlcycom@163.com>
2023-02-18 23:41:31 +0800

    添加硬盘读写操作，读出硬盘第一个主引导扇区的内容

--- source/boot.asm
+++ source/boot.asm
@@ -11,14 +11,89 @@ mov es, ax
 mov ss, ax
 mov sp, 0x7c00
 
-; bochs 魔术断点
-xchg bx, bx
-
 mov si, booting
 call print
 
+mov edi, 0x1000 ; 读取到的目标内存地址
+mov ecx, 0 ; 起始扇区
+mov bl, 1 ; 读取扇区数量
+
+; bochs 魔术断点
+xchg bx, bx
+
+call read_disk
+
 ; 阻塞
 jmp $
+
+read_disk:
+    ; 设置读写扇区的数量
+    mov dx, 0x1f2
+    mov al, bl
+    out dx, al
+
+    inc dx ; 0x1f3
+    mov al, cl ; 起始扇区的低八位
+    out dx, al
+
+    inc dx ; 0x1f4
+    shr ecx, 8
+    mov al, cl ; 起始扇区的中八位
+    out dx, al
+
+    inc dx ; 0x1f5
+    shr ecx, 8
+    mov al, cl ; 起始扇区的高八位
+    out dx, al
+
+    inc dx ; 0x1f6
+    shr ecx, 8 ; ECX 32bit
+    and cl, 0b1111 ; 清空高四位
+
+    mov al, 0b1110_0000
+    or al, cl
+    out dx, al ; 主盘 LBA模式
+
+    inc dx ; 0x1f7
+    mov ax, 0x20 ; 读硬盘
+    out dx, al
+
+    xor ecx, ecx ; 清空ECX
+    mov cl, bl ; 得到读写扇区的数量
+
+    .read:
+        push cx
+        call .waits ; 等待数据准备完毕
+        call .reads ; 读写一个扇区
+        pop cx
+        loop .read
+
+    ret
+
+    .waits:
+        mov dx, 0x1f7
+        .check:
+            in al, dx
+            jmp $+2 ; 直接跳转到下一行，原因是一条指令占用两个字节
+            jmp $+2 ; 相比与nop，延迟更长一些
+            jmp $+2
+            and al, 0b1000_1000
+            cmp al, 0b0000_1000
+            jnz .check
+        ret
+
+    .reads:
+        mov dx, 0x1f0
+        mov cx, 256
+        .readw:
+            in ax, dx
+            jmp $+2
+            jmp $+2
+            jmp $+2
+            mov [edi], ax
+            add edi, 2
+            loop .readw
+        ret
 
 print:
     mov ah, 0x0e
