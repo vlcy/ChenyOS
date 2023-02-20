99fe859d1f790f3a3409906f3cb56dd435b3a2b1
vlcy <vlcycom@163.com>
2023-02-21 01:07:44 +0800

    实现存检测部分

--- source/load.asm
+++ source/load.asm
@@ -2,10 +2,58 @@
 
 dw 0x55aa ; 魔数，用于判断内核加载器是否有效
 
+mov si, loading
+call print
+
 xchg bx, bx
 
-mov si, Loading
-call print
+detect_memory:
+    xor ebx, ebx
+
+    ; es:di 结构体的缓存位置
+    mov ax, 0
+    mov es, ax
+    mov edi, ards_buffer
+
+    mov edx, 0x534d4150 ; 固定签名
+
+.next:
+    ; 子功能号
+    mov eax, 0xe820
+    ; ards结构体大小
+    mov ecx, 20
+    ; 调用系统调用
+    int 0x15
+
+    ; CF置位，调用出错
+    jc error
+    
+    ; 将缓存指针指向下一个结构体
+    add di, cx
+
+    ; 将结构体数量加1
+    inc word [ards_count]
+
+    ; 最后一个结构体
+    cmp ebx, 0
+    jnz .next
+
+    mov si, detecting
+    call print
+
+;     xchg bx, bx
+
+;     ; 结构体数量
+;     mov cx, [ards_count]
+;     ; 结构体指针
+;     mov si, 0
+; .show:
+;     mov eax, [ards_buffer + si]
+;     mov ebx, [ards_buffer + si + 8]
+;     mov edx, [ards_buffer + si + 16]
+;     add si, 20
+;     xchg bx, bx
+;     loop .show
 
 jmp $
 
@@ -21,5 +69,20 @@ print:
 .done:
     ret
 
-Loading:
+loading:
     db "Loading ChenyOS...", 10, 13, 0 ; \n\t
+
+detecting:
+    db "Detecting Memory Success...", 10, 13, 0
+
+error:
+    mov si, .msg
+    call print
+    hlt ; 让CPU停止运行
+    jmp $
+    .msg db "Loading Error!!!", 10, 13, 0
+
+; ARDS 地址范围描述符结构
+ards_count:
+    dw 0
+ards_buffer:
