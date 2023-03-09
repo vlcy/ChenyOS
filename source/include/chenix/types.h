0355cf38dde8faa9bc5b6a34c3323c5062925eab
vlcy <vlcycom@163.com>
2023-03-10 00:31:42 +0800

    添加基本数据类型

---
+++ source/include/chenix/types.h
@@ -0,0 +1,25 @@
+#ifndef CHENIX_TYPES_H
+#define CHENIX_TYPES_H
+
+#define EOF -1
+#define NULL 0
+
+#define bool _Bool
+#define true 1
+#define false 0
+
+#define _packed __attribute__((packed))
+
+typedef unsigned int size_t;
+
+typedef char i8;
+typedef short i16;
+typedef int i32;
+typedef long long i64;
+
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned int u32;
+typedef unsigned long long u64;
+
+#endif
