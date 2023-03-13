1a97c91b7b1a026bfbee046f5ec2b84c22952e1b
vlcy <vlcycom@163.com>
2023-03-13 23:33:46 +0800

    添加IO功能文件

---
+++ source/kernel/io.asm
@@ -0,0 +1,67 @@
+[bits 32]
+
+section .text
+
+global inb
+inb:
+    push ebp
+    mov ebp, esp
+
+    xor eax, eax
+    mov edx, [ebp + 8]
+    in al, dx
+
+    jmp $ + 2
+    jmp $ + 2
+    jmp $ + 2
+
+    leave
+    ret
+
+global outb
+outb:
+    push ebp
+    mov ebp, esp
+
+    mov edx, [ebp + 8]
+    mov eax, [ebp + 12]
+    out dx, al
+
+    jmp $ + 2
+    jmp $ + 2
+    jmp $ + 2
+
+    leave
+    ret
+
+global inw
+inw:
+    push ebp
+    mov ebp, esp
+
+    xor eax, eax
+    mov edx, [ebp + 8]
+    in ax, dx
+
+    jmp $ + 2
+    jmp $ + 2
+    jmp $ + 2
+
+    leave
+    ret
+
+global outw
+outw:
+    push ebp
+    mov ebp, esp
+
+    mov edx, [ebp + 8]
+    mov eax, [ebp + 12]
+    out dx, ax
+
+    jmp $ + 2
+    jmp $ + 2
+    jmp $ + 2
+
+    leave
+    ret
--- source/kernel/main.c
+++ source/kernel/main.c
@@ -1,14 +1,20 @@
 #include <chenix/chenix.h>
+#include <chenix/types.h>
+#include <chenix/io.h>
 
-int magic = CHENIX_MAGIC_CODE;
-char message[] = "hello onix!!!"; // .data
-char buf[1024];                   // .bss
+
+#define CRT_ADDR_REG 0x3d4
+#define CRT_DATA_REG 0x3d5
+
+#define CRT_CURSOR_H 0xe
+#define CRT_CURSOR_L 0xf
 
 void kernel_init()
 {
-    char *video = (char *) 0xb8000; // 文本显示器的内存位置
-    for (int i = 0; i < sizeof(message); i++)
-    {
-        video[i * 2] = message[i];
-    }
+    outb(CRT_ADDR_REG, CRT_CURSOR_H);
+    u16 pos = inb(CRT_DATA_REG) << 8;
+    outb(CRT_ADDR_REG, CRT_CURSOR_L);
+    pos |= inb(CRT_DATA_REG);
+
+    return;
 }
